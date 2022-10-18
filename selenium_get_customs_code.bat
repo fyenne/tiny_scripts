5323e222ccf8dcfb1f07480cfd3cd7cdf5803c5c
fyenneyenn <33649824+fyenne@users.noreply.github.com>
2022-10-18 11:29:49 +0200

    notes

---
+++ selenium_get_customs_code.bat
@@ -0,0 +1,4 @@
+call conda activate siming
+echo ------------activated condad env------------                        
+call python "C:\Users\SRV-USAMYAN\Documents\My SugarSync\tiny_scirpts\selenium_customs_code_good.py"
+pause
