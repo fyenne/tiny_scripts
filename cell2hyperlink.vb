a1b53a81e733ef5ff1dc5de4ce1e3c2123213a3d
fyenneyenn <33649824+fyenne@users.noreply.github.com>
2022-10-20 05:57:39 +0200

    add

---
+++ cell2hyperlink.vb
@@ -0,0 +1,28 @@
+Sub createLink()
+
+    Dim x As Integer
+    Dim LastRow As Integer
+    
+    Dim regexOne As Object
+    Set regexOne = New RegExp
+    regexOne.Pattern = "\="
+    regexOne.Global = False
+    regexOne.IgnoreCase = True
+    Dim str As Object
+    Dim a As Object
+    
+    LastRow = Cells(Rows.Count, "A").End(xlUp).Row
+'    myNum = Application.InputBox("Enter a sheetname for oringinal reference")
+    
+    With ThisWorkbook.Worksheets("Sheet2")
+        For x = 1 To LastRow
+            'Debug.Print regexOne.Replace(ActiveCell.Formula, "")
+            Cells(x, Mid(ActiveCell.Address, 2, 1)).Activate
+            .Hyperlinks.Add Anchor:=Selection, Address:="", _
+            SubAddress:=regexOne.Replace(ActiveCell.Formula, ""), TextToDisplay:="Main Sheet"
+        Next x
+    End With
+    
+End Sub
+
+
