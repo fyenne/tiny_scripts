4d7dd829cb1d0649df1caec61d2bdf3aa8bae77f
samoyann <samo.yan@bunge.com>
2022-11-03 03:20:54 +0100

    add

--- auto_wow.py
+++ auto_wow.py
@@ -16,26 +16,30 @@ for i in a:
     time.sleep(1)
     pg.press('q')
     if i <= 3:
-        pg.press('tab')
+        pg.press('tab',)
         pg.press('e')
         pg.press('s')
 
     elif i <= 7:
-        pg.press('tab')
+        pg.press('tab', presses = 2)
         pg.press('q')
     else:
-        pg.press('w')
+        pg.press('w'  , presses = 2)
         pg.press('tab')
         pg.press('f')
     time.sleep(i)    
     pg.click(1005,505-i)
     time.sleep(i)
 
-    pg.press('q')
+    pg.press('q',interval=.4)
     pg.press('4')
     pg.click(1105,505+i)
     b+=i
     if b >= 55:
         pg.keyDown('shift')    
-        pg.press('1')  
+        pg.press('t')
+        time.sleep(i)  
         pg.keyUp('shift')
+        b=0
+    else:
+        pass
--- get_cred_remote_server.bat
+++ get_cred_remote_server.bat
@@ -1,4 +1,10 @@
+setlocal
+%@Try%
 call conda activate siming
-echo ------------activated condad env------------                        
+echo ------------activated conda env----------------                       
 call python "C:\Users\SRV-USAMYAN\Documents\My SugarSync\tiny_scirpts\get_cred_remote_server.py"
+%@EndTry%
+:@Catch
+echo --failed--
+:@EndCatch
 timeout 4
